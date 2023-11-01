38695aaa7bc8adfebf2eea7b1b6ee0abfea05f8c
ninaflinn <142847433+ninaflinn@users.noreply.github.com>
2023-11-01 17:35:13 -0400

    added code for user to input email

--- app/email_service.py
+++ app/email_service.py
@@ -22,20 +22,7 @@ def send_email(recipient_address=SENDER_ADDRESS, subject="[Shopping Cart App] Te
     
     #client = SendGridAPIClient(SENDGRID_API_KEY)
     #print("CLIENT:", type(client))
-#
-    #message = Mail(from_email=SENDER_ADDRESS, to_emails=recipient_address, subject=subject, html_content=html_content)
-#
-    #try:
-    #    response = client.send(message)
-#
-    #    print("RESPONSE:", type(response)) #> <class 'python_http_client.client.Response'>
-    #    print(response.status_code) #> 202 indicates SUCCESS
-    #    print(response.body)
-    #    print(response.headers)
-#
-    #except Exception as err:
-    #    print(type(err))
-    #    print(err)
+
 
 
 
@@ -61,4 +48,7 @@ my_content = """
         <li>Strawberry</li>
     </ul>
 """
+
+user_address = input("Please enter your email address: ")
+send_email(html_content=my_content, recipient_address=user_address)
 send_email(html_content=my_content)
